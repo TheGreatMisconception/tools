0c9102cdff5b1b2867af1ed319ee77606502ffcf
TheGreatMisconception <114302667+TheGreatMisconception@users.noreply.github.com>
2023-04-10 12:09:08 +0200

    Add welcome.sh profile script

--- Linux/Linux.md
+++ Linux/Linux.md
@@ -5,18 +5,10 @@ A collection of scripts i wrote to simplify my Linux-Journey.
 
 
 
-## ArchLinux.sh (TODO, does not work in it's current state!!!)
+## Welcome.sh
 
-A script that installs ArchLinux the way i want it to. This script is very subjectiv and most of the installation is hardcoded. If you're looking for a way to automate the installation of ArchLinux, you may want to check out "archinstall".
-
-> A working internet connection and superuser permissions are required!
-
-| Arguments | Description |
-|--|--|
-| -b |  basic installation only|
-|-g| graphical installation (gnome-desktop)|
-|-s| output information about the repository
-|-h| outputs a simple help dialog
+A simple 'Welcome' script. For optimal use copy that script into /etc/profile.d/ and make it executable (E.g `chmod +x welcome.sh`)
+Note: This script requires figlet to be installed.
 
 # License
 
--- Linux/welcome.sh
+++ Linux/welcome.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-if [ "$(whoami)" != "root" ]; then
-  
-  echo
-  figlet -w 500 -f slant "Welcome ${USER}"
-  echo
+function welcome() {
+  if [ "$(whoami)" != "root" ]; then
+    echo
+    figlet -w 500 -f slant "Welcome ${USER}"
+    echo
+  else
+    RED='\033[0;31m'
+    NO_COLOR='\033[0m'
+    echo "----------------------------------------------------------------"
+    echo -e " ${RED}Please try to prevent the login of root. Use \"sudo\" instead. ${NO_COLOR}"
+    echo "----------------------------------------------------------------"
+  fi
+}
 
+
+# Check if figlet is installed
+if command -v "figlet" &> /dev/null; then
+  welcome
 else
-
-  RED='\033[0;31m'
-  NO_COLOR='\033[0m'
-  echo "----------------------------------------------------------------"
-  echo -e " ${RED}Please try to prevent the login of root. Use \"sudo\" instead. ${NO_COLOR}"
-  echo "----------------------------------------------------------------"
-
+  echo "Can not display Welcome-Message beacuse 'figlet' is not installed. Consider installing the necessary package(s) or removing this script:"
+  echo $0
 fi
